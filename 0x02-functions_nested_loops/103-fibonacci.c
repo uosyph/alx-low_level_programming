bae0364e85a3fe146661e16e33c69fa775712f22
Yousef Saeed <yousafesaeed@gmail.com>
2023-02-22 03:56:48 +0200

    Formatting Fixes

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,6 +5,7 @@
  * purpose - fibonacci
  * Return: (0) always
  */
+
 int main(void)
 {
 unsigned long int lim = 4000000;
@@ -13,18 +14,19 @@ unsigned long int aft = 1;
 unsigned long int R = 0;
 unsigned long int sum = 0;
 
-    while (lim > R)
-																		{
-															R = bef + aft;
+while (lim > R)
+{
+	R = bef + aft;
+	if ((R % 2) == 0)
+	{
+		sum += R;
+	}
 
-    if ((R % 2) == 0)
-															{
-																														sum += R;
-																														}
+	bef = aft;
+	aft = R;
+}
 
-															bef = aft;
-																														aft = R;
-																																	}
-															printf("%li\n", sum);
-															return (0);
+printf("%li\n", sum);
+return (0);
+
 }
