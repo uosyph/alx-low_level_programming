44ede790c64ae2bb0ad068c43e2fd97843886360
Yousef Saeed <yousafesaeed@gmail.com>
2023-04-07 13:24:32 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,34 +2,33 @@
 
 /**
  * create_file - creates a file.
- * @filename: first argument, filename.
+ * @filename: first argument, the name of the file.
  * @text_content: second argument, the content written to the file.
  * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-    if (filename == NULL)
+    int filedata, content, length = 0;
+
+    if (filename == 0)
+        return (-1);
+
+    filedata = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+    if (filedata == -1)
+        return (-1);
+
+    if (text_content)
     {
-        return -1;
+        while (text_content[length])
+            length++;
+        content = write(filedata, text_content, length);
+
+        if (content == -1)
+            return (-1);
     }
 
-    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-    {
-        return -1;
-    }
+    close(filedata);
 
-    if (text_content != NULL)
-    {
-        ssize_t write_count = write(fd, text_content, strlen(text_content));
-        if (write_count == -1)
-        {
-            close(fd);
-            return -1;
-        }
-    }
-
-    close(fd);
-
-    return 1;
+    return (1);
 }
