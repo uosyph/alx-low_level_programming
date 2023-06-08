ecfd93922f8f4af6168778340f3232c3ea2b18d2
Yousef Saeed <yousafesaeed@gmail.com>
2023-06-08 19:59:28 +0300

    minor changes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,49 +9,43 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_t;
-	char *copy_value;
-	unsigned long int index, counter;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-	if (ht == NULL || key == NULL || value == NULL || *key == "\0")
-	{
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	}
 
-	copy_value = strdup(value);
-	if (copy_value == NULL)
-	{
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
-	}
 
 	index = key_index((const unsigned char *)key, ht->size);
-	for (counter = index; ht->array[counter]; counter++)
+	for (i = index; ht->array[i]; i++)
 	{
-		if (strcmp(ht->array[counter]->key, key) == 0)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			free(ht->array[counter]->value);
-			ht->array[counter]->value = copy_value;
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
 			return (1);
 		}
 	}
 
-	new_t = malloc(sizeof(hash_node_t));
-	if (new_t == NULL)
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
-		free(copy_value);
+		free(value_copy);
 		return (0);
 	}
-
-	new_t->key = strdup(key);
-	if (new_t->key == NULL)
+	new->key = strdup(key);
+	if (new->key == NULL)
 	{
-		free(new_t);
+		free(new);
 		return (0);
 	}
-
-	new_t->value = copy_value;
-	new_t->next = ht->array[index];
-	ht->array[index] = new_t;
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
 	return (1);
 }
