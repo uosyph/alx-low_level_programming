09de23fdcac82c139a826c1caedea81d8bea9414
Yousef Saeed <yousafesaeed@gmail.com>
2023-09-12 01:56:23 +0300

    Combine funcs

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,29 +1,6 @@
 #include "search_algos.h"
 
 /**
- * print_search_array - Prints the array that's being searched
- * @array: Pointer to the array being searched
- * @start: Index to start printing at
- * @end: Index to finish printing at
- **/
-void print_search_array(int *array, int start, int end)
-{
-	int i;
-
-	printf("Searching in array: ");
-
-	i = start;
-	while (i <= end)
-	{
-		if (i > start)
-			printf(", ");
-		printf("%d", array[i]);
-		i++;
-	}
-	printf("\n");
-}
-
-/**
  * binary_search - Uses a binary search to find the given value in an array
  * @array: Pointer to the first element of the array to search in
  * @size: Number of elements in array
@@ -32,22 +9,28 @@ void print_search_array(int *array, int start, int end)
  **/
 int binary_search(int *array, size_t size, int value)
 {
-	int left, right, compare;
+	size_t index, left, right;
 
-	left = compare = 0;
-	right = size - 1;
+	if (array == NULL)
+		return (-1);
 
-	while (left <= right)
+	for (left = 0, right = size - 1; right >= left;)
 	{
-		print_search_array(array, left, right);
+		printf("Searching in array: ");
 
-		compare = (left + right) / 2;
-		if (array[compare] == value)
-			return (compare);
-		else if (array[compare] > value)
-			right = compare;
-		else if (array[compare] < value)
-			left = compare + 1;
+		for (index = left; index < right; index++)
+			printf("%d, ", array[index]);
+
+		printf("%d\n", array[index]);
+
+		index = left + (right - left) / 2;
+		if (array[index] == value)
+			return (index);
+		if (array[index] > value)
+			right = index - 1;
+		else
+			left = index + 1;
 	}
+
 	return (-1);
 }
